97ccf4796b903d3c9f7c4893416489da1633a067
Kacper Stokwisz <kacp1@o2.pl>
2022-06-01 21:07:15 +0200

    Create database connection

---
+++ utils/db.ts
@@ -0,0 +1,10 @@
+import {createPool} from "mysql2/promise";
+
+export const pool = createPool({
+    host: 'localhost',
+    user: 'root',
+    database: 'fast_cleaning',
+    // password
+    namedPlaceholders: true,
+    decimalNumbers: true,
+});
