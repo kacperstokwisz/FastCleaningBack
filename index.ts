832d923836b0455882d945ff03eb2e90f1947171
Kacper Stokwisz <kacp1@o2.pl>
2022-06-04 16:42:41 +0200

    Add adRouter

--- index.ts
+++ index.ts
@@ -2,6 +2,8 @@ import express, { json } from "express";
 import cors from "cors";
 import "express-async-errors";
 import { handleError } from "./utils/errors";
+import { AdRecord } from "./records/ad.record";
+import { adRouter } from "./routers/ad.router";
 
 const app = express();
 
@@ -13,6 +15,16 @@ app.use(
 
 app.use(json());
 app.use(handleError);
+app.use("/ad", adRouter);
+
+// TESTS
+// const funkcja = async () => {
+//   const aaa = await AdRecord.findAll("Name");
+//   console.log(aaa);
+
+// };
+
+// funkcja();
 
 app.listen(3001, "0.0.0.0", () => {
   console.log("Listening on http://localhost:3001");
