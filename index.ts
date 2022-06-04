f103dd717cd2b43725e40c703eca12086d2a715e
Kacper Stokwisz <kacp1@o2.pl>
2022-06-04 17:17:26 +0200

    Install rate limiter

--- index.ts
+++ index.ts
@@ -2,6 +2,7 @@ import express, { json } from "express";
 import cors from "cors";
 import "express-async-errors";
 import { handleError } from "./utils/errors";
+import rateLimit from "express-rate-limit";
 import { AdRecord } from "./records/ad.record";
 import { adRouter } from "./routers/ad.router";
 
@@ -9,7 +10,14 @@ const app = express();
 
 app.use(
   cors({
-    origin: "https://localhost:3000",
+    origin: "http://localhost:3000",
+  })
+);
+
+app.use(
+  rateLimit({
+    windowMs: 5 * 60 * 1000, // 15 minutes
+    max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
   })
 );
 
